f388a9261429fd238ca0ebbbe5558c8167d52f24
ju-sh <ju-sh@github.io>
2019-07-05 14:40:54 +0530

    stage 1 complete

--- popup/edel.js
+++ popup/edel.js
@@ -1,32 +1,35 @@
-document.addEventListener('DOMContentLoaded', function() {
-    init_function()
-}, false);
-
-
 function logTabs(tabs) {
   const TAB_NAME_WIDTH = 25
+  var containerDiv = document.getElementById("container");
+/*
+  if (tabs.length==0) {
+    var noTabMsgDiv = document.createElement("div");
+    noTabMsgDiv.className = "table-row"; //class is a js keyword. So it's named className instead.   
+    noTabMsgDiv.innerHTML = "No tabs open.";
+    noTabMsgDiv.appendChild(tableRowDiv);
+  }
+*/
   for (let tab of tabs) {
-    console.log(tab.url);
-    console.log(tab.title);
-
-    var containerDiv = document.getElementById("container");
-
     var tableRowDiv = document.createElement("div");
     var checkboxDiv = document.createElement("div");
     var checkboxInput = document.createElement("input");
     var labelDiv = document.createElement("div");
     var label = document.createElement("label");
+    var urlAnchor = document.createElement("a");
 
-    tableRowDiv.class = "table-row";
-    checkboxDiv.class = "table-col";
-    labelDiv.class = "table-col";
-    label.class = "url-label";
+    tableRowDiv.className = "table-row"; //class is a js keyword. So it's named className instead.   
+    checkboxDiv.className = "table-col";
+    labelDiv.className = "table-col";
+    label.className = "url-label";
 
+    urlAnchor.href=tab.url;
     label.htmlFor = tab.id.toString(); //for is a js keyword. So it's named htmlFor instead.
-    label.innerHTML = tab.title;
+    urlAnchor.innerHTML = tab.title;
+    checkboxInput.className = "url-checkbox";
     checkboxInput.id = tab.id.toString();
     checkboxInput.type = "checkbox";
 
+    label.appendChild(urlAnchor);
     labelDiv.appendChild(label);
     checkboxDiv.appendChild(checkboxInput);
     tableRowDiv.appendChild(checkboxDiv); 
@@ -34,6 +37,30 @@ function logTabs(tabs) {
 
     containerDiv.appendChild(tableRowDiv);
   }
+}
+
+function checkOrUncheckBoxes() {
+    var allNoneCheckbox = document.getElementById("all-none-checkbox");
+    var allNoneLabel = document.getElementById("all-none-label");
+    if (allNoneCheckbox === null) {
+        console.log("Error: allNoneCheckbox is null!");
+        return;
+    }
+    if (allNoneLabel === null) {
+        console.log("Error: allNoneLabel is null!");
+        return;
+    }
+
+    if (allNoneCheckbox.checked) {
+        allNoneLabel.innerHTML = "Clear";    
+    } else {
+        allNoneLabel.innerHTML = "Select all";    
+    }
+    var checkbox_array = document.getElementsByClassName("url-checkbox");
+    for (let checkbox of checkbox_array) {
+        console.log(checkbox);
+        checkbox.checked = allNoneCheckbox.checked;
+    }
 }
 
 function onError(error) {
@@ -45,4 +72,7 @@ function init_function() {
     querying.then(logTabs, onError);
 }
 
-window.onload = init_function();
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById("all-none-checkbox").addEventListener("click", checkOrUncheckBoxes); //what if all-none-checkbox is null????
+    init_function()
+}, false);
